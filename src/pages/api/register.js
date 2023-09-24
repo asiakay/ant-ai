a07cfc4c14100e0a043eec72b53c11f51ae69bf2
Asia K <66960776+asiakay@users.noreply.github.com>
2023-09-24 02:41:37 -0400

    import the createUser function

--- src/pages/api/register.js
+++ src/pages/api/register.js
@@ -1,8 +1,31 @@
+// register.js
+const { createUser } = require('../../utils/db');
+
 export default async function handler(req, res) {
     if (req.method !== 'POST') {
             // Parse the incoming JSON data
             try {
                 const data = JSON.parse(req.body);
+
+                      // Validate the data (e.g., check for required fields)
+
+      // Create the user record
+      const newUser = createUser(data);
+
+        // Return the user's data
+        res.status(200).json(newUser);
+    } catch (error) {
+        console.error('Registration error:', error);
+        res.status(500).json({ error: 'Unable to register user' });
+    }
+} else {
+    // Handle any other HTTP method
+    res.status(405).end(); // Method Not Allowed
+    }
+}
+
+
+
 
     // Here, you would typically handle user creation, database interaction, and validation
     // Example: Create a user in your database
@@ -12,7 +35,7 @@ export default async function handler(req, res) {
     // Replace this with your actual registration logic
 
     // Return the user's data
-    const newUser = {
+/*     const newUser = {
         username: data.username,
         email: data.email,
         age: data.age,
@@ -20,17 +43,4 @@ export default async function handler(req, res) {
         goals: data.goals,
         dietaryRestrictions: data.dietaryRestrictions,
     };
-
-        // Return a success response
-
-    res.status(200).json(newUser);
-} catch (error) {
-    console.error('Registration error:', error);
-    res.status(500).json({ error: 'Unable to register user' });
-}
-} else {
-    // Handle any other HTTP method
-    res.status(405).end(); // Method Not Allowed
-
-    }
-}
+ */
